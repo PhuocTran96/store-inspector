8477042974d7459d4d4ec18bf42e1e3563d73b72
Phuoctran96 <tranhoangphuoc18@gmail.com>
2025-06-08 05:39:21 +0700

    Prepare for Heroku deployment: Add Procfile, update package.json, enhance .gitignore, add app.json

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: { 
-    secure: false, // set to true in production with HTTPS
+    secure: process.env.NODE_ENV === 'production', // HTTPS in production
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
     httpOnly: true
   }
